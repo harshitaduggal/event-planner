5091a3445618e69f46888d0d1cb785f19dbb08aa
harshitaduggal <148007874+harshitaduggal@users.noreply.github.com>
2025-05-31 09:45:01 +0530

    Add files via upload

---
+++ EventPlannerTest.java
@@ -0,0 +1,18 @@
+// EventPlannerTest.java
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class EventPlannerTest {
+
+    @Test
+    public void testAddEvent() {
+        String title = "Birthday";
+        assertEquals("Birthday", title);  // dummy test for now
+    }
+
+    @Test
+    public void testSample() {
+        int a = 2 + 2;
+        assertEquals(4, a);
+    }
+}
